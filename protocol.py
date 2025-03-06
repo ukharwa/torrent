dbe79b28d4ece7f245796dc1e5dfe15e58826806
Umar Kharwa <umar.kharwa@gmail.com>
2025-03-06 13:08:12 +0200

    rename

--- protocol.py
+++ protocol.py
@@ -1,53 +1,39 @@
 import socket
 
-class Protocol:
-    def __init__(self, socket=None):
-        self.data = bytearray(260)
-        self.socket = socket
-
-    def post(self, ip, port):
-        self.socket.sendto(self.data, (ip, port))
-        self.data = bytearray(260)
+class Request:
+    def __init__(self):
+        self.data = bytearray()
     
-    def seed(self, file, ip, port):
-        self.data[0] = 3
-        self.data[1:5] = len(file).to_bytes(4, 'little')
-        self.data[5:] = file.encode()
-        self.post(ip, port)
+    def connection_request(self):
+        data = bytearray(4)
+        action = 0
+        data[0:4] = action.to_bytes(4, 'little')
+        return data
     
-    def leech(self, file, ip, port):
-        self.data[0] = 2
-        self.data[1:5] = len(file).to_bytes(4, 'little')
-        self.data[5:] = file.encode()
-        self.post(ip, port)
+    def connection_response(self):
+        data = bytearray(4)
+        action = 0
+        data[0:4] = action.to_bytes(4, 'little')
+        return data
     
-    def sendMessage(self, message, ip, port):
-        self.data[0] = 0
-        self.data[1:5] = len(message).to_bytes(4, 'little')
-        self.data[5:] = message.encode()
-        self.post(ip, port)
+    def announce_request(self, torrent, peerID, downloaded, uploaded, left, event, ip, port):
+        data = bytearray(78)
+        action = 1
+        data[0:4] = action.to_bytes(4, 'little')
+        data[4:24] = torrent.encode()
+        data[24:44] = peerID.encode()
+        data[44:52] = downloaded.to_bytes(8, 'little')
+        data[52:60] = uploaded.to_bytes(8, 'little')
+        data[60:68] = left.to_bytes(8, 'little')
+        data[68:72] = event.to_bytes(4, 'little')
+        data[72:76] = ip.to_bytes(4, 'little')
+        data[76:] = port.to_bytes(2, 'little')
+        return data
 
-    def sendIP(self, Mip, Mport, ip, port):
-        self.data[0] = 1
-        self.data[1:5] = len(Mip).to_bytes(4, "little")
-        self.data[5:9] = Mport.to_bytes(4, "little")
-        self.data[9:] = Mip.encode()
-        self.post(ip, port)
-
-    def receive(self, data):
-        type = data[0]
-        length = int.from_bytes(data[1:5], 'little')
-
-        if type == 0:
-            message = data[5:9+length].decode()
-            print(message)
-            return type, message
-        
-        if type == 1:
-            port = int.from_bytes(data[5:9], "little")
-            ip = data[9:9+length].decode()
-            return type, (ip, port)
-        
-        if type == 2:
-            file = data[5:9+length].decode()
-            return type, file
+    def announce_response(self, interval, leechers, seeders, peers):
+        data = bytearray(20)
+        action = 1
+        data[0:4] = action.to_bytes(4, 'little')
+        data[4:12] = interval.to_bytes(8, 'little')
+        data[12:16] = leechers.to_bytes(4, 'little')
+        data[12:16] = seeders.to_bytes(4, 'little')
---
+++ protocol_old.py
@@ -0,0 +1,53 @@
+import socket
+
+class Protocol:
+    def __init__(self, socket=None):
+        self.data = bytearray(260)
+        self.socket = socket
+
+    def post(self, ip, port):
+        self.socket.sendto(self.data, (ip, port))
+        self.data = bytearray(260)
+    
+    def seed(self, file, ip, port):
+        self.data[0] = 3
+        self.data[1:5] = len(file).to_bytes(4, 'little')
+        self.data[5:] = file.encode()
+        self.post(ip, port)
+    
+    def leech(self, file, ip, port):
+        self.data[0] = 2
+        self.data[1:5] = len(file).to_bytes(4, 'little')
+        self.data[5:] = file.encode()
+        self.post(ip, port)
+    
+    def sendMessage(self, message, ip, port):
+        self.data[0] = 0
+        self.data[1:5] = len(message).to_bytes(4, 'little')
+        self.data[5:] = message.encode()
+        self.post(ip, port)
+
+    def sendIP(self, Mip, Mport, ip, port):
+        self.data[0] = 1
+        self.data[1:5] = len(Mip).to_bytes(4, "little")
+        self.data[5:9] = Mport.to_bytes(4, "little")
+        self.data[9:] = Mip.encode()
+        self.post(ip, port)
+
+    def receive(self, data):
+        type = data[0]
+        length = int.from_bytes(data[1:5], 'little')
+
+        if type == 0:
+            message = data[5:9+length].decode()
+            print(message)
+            return type, message
+        
+        if type == 1:
+            port = int.from_bytes(data[5:9], "little")
+            ip = data[9:9+length].decode()
+            return type, (ip, port)
+        
+        if type == 2:
+            file = data[5:9+length].decode()
+            return type, file
