6379f0855fd4b586877c94eabcb0ed819f7c0b23
Umar Kharwa <umar.kharwa@gmail.com>
2025-03-10 15:55:44 +0200

    leeching and seeding functions

--- client.py
+++ client.py
@@ -6,58 +6,61 @@ udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 tracker_ip = "196.42.75.186"
 tracker_port = 6969
-client_ip = socket.gethostbyname(socket.gethostname())
-client_port = 9001
-peerID = generate_peerid(client_ip, "Hello")
 
 protocol = Request()
 
-#decode .ppp into variables
-#take in a file.ppp 
-with open(input("Enter torrent file:"),"r") as torrent_file:
-    torrent_data = json.load(torrent_file)
-    actual_file_size = torrent_data["file_size"]
-    file_hash = torrent_data["file_hash"]
-    packet_list = torrent_data["packet_list"]
-
-downloaded = 0
-uploaded = 0
-left = 0
-error_count = 0
-
-while True:
-    if error_count == 5:
-        print("ERROR: Too many errors, exiting...")
-        break
-    print("Searching for tracker...")
-    udp_client.sendto(protocol.connection_request(), (tracker_ip, tracker_port))
-    data, _ = udp_client.recvfrom(12)
-    action = int.from_bytes(data[0:4], 'little')
-
-    if action == 0:
-        print("Tracker found")
-        response = protocol.client_decode(data)
-        connectionID = response["connectionID"]
-
-        print("Attempting connection...")
-        announce = protocol.announce_request(connectionID, file_hash, peerID, 0, 0, left, 1, client_ip, 9000)
-        udp_client.sendto(announce, (tracker_ip, tracker_port))
-
-        data, _ = udp_client.recvfrom(1024)
-        response = protocol.client_decode(data)
-        action = int.from_bytes(data[0:4], 'little')       
-        
-        if action == 99:
-            print("ERROR: " + response["ERROR"])
-            error_count += 1
-        if action == 1:
-            print("Successfully connected to tracker...")
-            print(response)
+def connect_to_tracker(file_hash, downloaded, uploaded, left, ip, port):
+    while True:
+        if error_count == 5:
+            print("ERROR: Too many errors, exiting...")
             break
+        print("Searching for tracker...")
+        udp_client.sendto(protocol.connection_request(), (tracker_ip, tracker_port))
+        data, _ = udp_client.recvfrom(12)
+        action = int.from_bytes(data[0:4], 'little')
 
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if action == 0:
+            print("Tracker found")
+            response = protocol.client_decode(data)
+            connectionID = response["connectionID"]
 
-def leech(response):
+            print("Attempting connection...")
+            announce = protocol.announce_request(connectionID, file_hash, generate_peerid(ip, "Hello"), downloaded, uploaded, left, 1, ip, port)
+            udp_client.sendto(announce, (tracker_ip, tracker_port))
+
+            data, _ = udp_client.recvfrom(1024)
+            response = protocol.client_decode(data)
+            action = int.from_bytes(data[0:4], 'little')       
+            
+            if action == 99:
+                print("ERROR: " + response["ERROR"])
+                error_count += 1
+            if action == 1:
+                print("Successfully connected to tracker...")
+                print(response)
+                break
+    return response
+
+
+def leech(filename):
+    client_ip = socket.gethostbyname(socket.gethostname())
+    client_port = 9001
+
+    with open(filename,"r") as torrent_file:
+        torrent_data = json.load(torrent_file)
+        actual_file_size = torrent_data["file_size"]
+        file_hash = torrent_data["file_hash"]
+        packet_list = torrent_data["packet_list"]
+    torrent_file.close()
+
+    downloaded = 0
+    uploaded = 0
+    left = 1000
+
+    response = connect_to_tracker(file_hash, downloaded, uploaded, left, client_ip, client_port)
+
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     seeders = response["seeders"]
     #list of seeders :(ip,port)
     for s in seeders:
@@ -79,7 +82,25 @@ def leech(response):
             new_file.write(p)
     new_file.close()
 
-def seed(filename):
+
+def seed(torrent_file, filename):
+    client_ip = socket.gethostbyname(socket.gethostname())
+    client_port = 9001
+
+    with open(torrent_file,"r") as torrent_file:
+        torrent_data = json.load(torrent_file)
+        actual_file_size = torrent_data["file_size"]
+        file_hash = torrent_data["file_hash"]
+        packet_list = torrent_data["packet_list"]
+    torrent_file.close()
+
+    downloaded = 0
+    uploaded = 0
+    left = 0
+
+    response = connect_to_tracker(file_hash, downloaded, uploaded, left, client_ip, client_port)
+
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_client.bind((client_ip,client_port))
     tcp_client.listen()
 
@@ -94,3 +115,9 @@ def seed(filename):
         conn.send(packets[packet_hash])
     
     tcp_client.close()
+
+
+torrent_file = input("Enter the .ppp file name: ")
+file = input("Enter the file name: ")
+
+leech(torrent_file)
