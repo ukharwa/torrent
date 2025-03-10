5f11b3d79f7e07460bfc12086b371135c107ec92
Umar Kharwa <umar.kharwa@gmail.com>
2025-03-10 14:02:03 +0200

    changes

--- tracker.py
+++ tracker.py
@@ -13,7 +13,6 @@ protocol = Request()
 print("Tracker online")
 
 while True:
-    print(files)
     data, addr = tracker.recvfrom(1024)
     action = int.from_bytes(data[0:4], "little")
 
@@ -27,19 +26,26 @@ while True:
         print("Announcement received from " + addr[0])
         response = protocol.tracker_decode(data)
         connectionID = response["connectionID"]
+
         if decode_connectionID(connectionID) in connections:
             print("connectionID found")
             peer = peer_from_announce(response)
             file = response["file_hash"]
+
             if response["left"] > 0:
-                files[file]["leechers"][response["peerID"]] = peer
+                if file in files:
+                    files[file]["leechers"][response["peerID"]] = peer
+                else:
+                    tracker.sendto(protocol.send_error("No seeders for this file"), addr)
             else:
-                if file in  files:
+                if file in files:
                      files[file]["seeders"][response["peerID"]] = peer
                 else:
                     files[file] = dict(seeders = {response["peerID"] : peer}, leechers = {})
+
             print("Peer " + response["peerID"] + " connected")
             tracker.sendto(protocol.announce_response(300, len(files[file]["leechers"]), len(files[file]["seeders"]), list(files[file]["seeders"].values())), addr)
+
         else:
             print("Invalid connectionID received")
             tracker.sendto(protocol.send_error("Invalid connectionID"), addr)
