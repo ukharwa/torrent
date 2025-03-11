cef331475cf59bf448a8f1ed0dc4d4c3fbb5b739
Kaamil Saib <kaamilsaib01@gmail.com>
2025-03-11 16:32:17 +0200

    gui eh

--- testgui.py
+++ testgui.py
@@ -1,39 +1,54 @@
 import tkinter as tk
+from tkinter.ttk import *
 from tkinter import filedialog
 import json
-import generate_torrent as torrent
 
 # Create the main window
 root = tk.Tk()
 root.title("App")
 root.geometry("500x700")  # Width x Height
-label = tk.Label(root,text="PP Protocol", font=("Arial",14))
+label = tk.Label(root, text="PP Protocol", font=("Arial", 14))
 label.pack(pady=10)
 
 def open_file():
-    
     file_path = filedialog.askopenfilename(title="Select A File", filetypes=(("PPP Files", "*.ppp"),))
   
     if file_path:
         print(f"Selected file: {file_path}")
 
         with open(file_path, "r") as file:
-             data = json.load(file)
+            data = json.load(file)
 
         text_box.insert(tk.END, data["info hash"]) 
     else:
-            print("No file selected")
+        print("No file selected")
 
-button = tk.Button(root,text="Seed a file",
-                   font=("Helvetica",11),
-                   bg= "green",
-                   fg="white",
-                   padx=5, pady=5,
-                   relief="flat",
-                   border=0,
-                   command=open_file)
-button.pack(pady=5)
-text_box = tk.Text(height=10,width=50)
+# Frame to hold buttons horizontally
+button_frame = tk.Frame(root)
+button_frame.pack(pady=5)
+
+upload_button = tk.Button(button_frame, text="Upload torrent file",
+                          font=("Helvetica", 11),
+                          bg="#2d9407", fg="white",
+                          padx=5, pady=5,
+                          relief="flat",
+                          activebackground="green",
+                          activeforeground="white",
+                          border=0,
+                          command=open_file)
+upload_button.pack(side=tk.LEFT, padx=5)  # Place to the left
+
+cancel_button = tk.Button(button_frame, text="Cancel",
+                          font=("Helvetica", 11),
+                          bg="#d60202", fg="white",
+                          padx=5, pady=5,
+                          relief="flat",
+                          activebackground="#a10303",
+                          activeforeground="white",
+                          border=0)
+cancel_button.pack(side=tk.LEFT, padx=5)  # Place next to upload button
+
+text_box = tk.Text(root, height=10, width=50)
 text_box.pack(pady=10)
 
 # Run the application
