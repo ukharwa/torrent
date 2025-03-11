588df4f8d074fd347eef96893964bce06548b4fc
Kaamil Saib <kaamilsaib01@gmail.com>
2025-03-11 14:33:06 +0200

    testgui

--- torrent_file/testgui.py
+++ torrent_file/testgui.py
@@ -4,22 +4,23 @@ import testfilespliting as torrent
 
 # Create the main window
 root = tk.Tk()
-root.title("My First Tkinter App")
+root.title("App")
 root.geometry("500x700")  # Width x Height
 label = tk.Label(root,text="Seeder", font=("Arial",14))
 label.pack(pady=10)
 
 def open_file():
     
-    file_path = filedialog.askopenfilename(title="Select a File",
-                                           filetypes=[("All Files", "*.*"))
+    file_path = filedialog.askopenfilename(title="Select A File", filetypes=(("All Files", "*.*"),))
     
-    print(file_path)
-    if file_path:  # If a file is selected
-        torrent_data = torrent.filetotorrent(file_path)
+  
+    if file_path:
+        print(f"Selected file: {file_path}")
+        torrent_data = torrent.gettorrentdata(file_path)
         print(torrent_data)
-        text_box.insert(tk.END, torrent_data)  # Display content in text widget
-
+        text_box.insert(tk.END, torrent_data) 
+    else:
+            print("No file selected")
 
 button = tk.Button(root,text="Seed a file",command=open_file)
 button.pack(pady=5)
