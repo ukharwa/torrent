0f1e070ad2152ff3d8ec069b7f8b863a386d7bc9
ukharwa <umar.kharwa@gmail.com>
2025-03-12 15:43:29 +0200

    commit

--- tracker.py
+++ tracker.py
@@ -12,43 +12,52 @@ connections = {}
 protocol = Request()
 print("Tracker online")
 
-while True:
-    data, addr = tracker.recvfrom(1024)
-    action = int.from_bytes(data[0:4], "little")
+def await_message():
+    while True:
+        data, addr = tracker.recvfrom(1024)
+        action = int.from_bytes(data[0:4], "little")
 
-    if action == 0:
-        print("Connection request from " + addr[0] +  " receivied...")
-        connectionID = get_connectionID(addr[0])
-        connections[decode_connectionID(connectionID)] = time.time()
-        tracker.sendto(protocol.connection_response(connectionID), addr)
+        if action == 0:
+            print("Connection request from " + addr[0] +  " receivied...")
+            connectionID = get_connectionID(addr[0])
+            connections[decode_connectionID(connectionID)] = time.time()
+            tracker.sendto(protocol.connection_response(connectionID), addr)
 
-    if action == 1:
-        print("Announcement received from " + addr[0])
-        response = protocol.tracker_decode(data)
-        connectionID = response["connectionID"]
+        if action == 1:
+            print("Announcement received from " + addr[0])
+            response = protocol.tracker_decode(data)
+            connectionID = response["connectionID"]
 
-        if decode_connectionID(connectionID) in connections:
-            print("connectionID found")
-            peer = peer_from_announce(response)
-            file = response["file_hash"]
+            if decode_connectionID(connectionID) in connections:
+                print("connectionID found")
+                file = response["file_hash"]
 
-            if response["left"] > 0:
-                if file in files:
-                    files[file]["leechers"][response["peerID"]] = peer
-                else:
-                    tracker.sendto(protocol.send_error("No seeders for this file"), addr)
-                    continue
+                if response["event"] == 1:
+                    peer = peer_from_announce(response)
+
+                    if response["left"] > 0:
+                        if file in files:
+                            files[file]["leechers"][response["peerID"]] = peer
+                        else:
+                            tracker.sendto(protocol.send_error("No seeders for this file"), addr)
+                            continue
+                    else:
+                        if file in files:
+                             files[file]["seeders"][response["peerID"]] = peer
+                        else:
+                            files[file] = dict(seeders = {response["peerID"] : peer}, leechers = {})
+                    print("Peer " + response["peerID"] + " connected")
+                    tracker.sendto(protocol.announce_response(300, len(files[file]["leechers"]), len(files[file]["seeders"]), list(files[file]["seeders"].values())), addr)
+                if response["event"] == 2:
+
+
             else:
-                if file in files:
-                     files[file]["seeders"][response["peerID"]] = peer
-                else:
-                    files[file] = dict(seeders = {response["peerID"] : peer}, leechers = {})
+                print("Invalid connectionID received")
+                tracker.sendto(protocol.send_error("Invalid connectionID"), addr)
 
-            print("Peer " + response["peerID"] + " connected")
-            tracker.sendto(protocol.announce_response(300, len(files[file]["leechers"]), len(files[file]["seeders"]), list(files[file]["seeders"].values())), addr)
-
-        else:
-            print("Invalid connectionID received")
-            tracker.sendto(protocol.send_error("Invalid connectionID"), addr)
+def cleanup():
+    for conn, t in connections:
+        if (time.time()).__trunc__() - t > 900:
+            connections.pop(conn)
 
 
